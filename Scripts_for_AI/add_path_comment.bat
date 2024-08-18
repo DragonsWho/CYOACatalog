37762388703a1f563d18091c8bc4eba0047644b7
DragonsWho <123765425+DragonsWho@users.noreply.github.com>
2024-08-19 01:14:28 +0300

    Delete Scripts_for_AI directory
    fix

--- Scripts_for_AI/add_path_comment.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -ExecutionPolicy Bypass -File "%~dp0\.\add_path_comment.ps1"
--- Scripts_for_AI/run_generate_structure.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -ExecutionPolicy Bypass -File "%~dp0\generate_src_structure.ps1"

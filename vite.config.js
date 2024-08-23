3b2166bf1016641c5ee80879106e7e9f56d09d03
root <root@vm2928081.stark-industries.solutions>
2024-08-23 17:50:20 +0200

    Server configuration updates

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import commonjs from 'vite-plugin-commonjs'
 
 export default defineConfig({
-    plugins: [
-        react(),
-        nodePolyfills({
-            globals: {
-                global: true,
-            },
-        }),
-    ],
+  plugins: [
+    react(),
+    nodePolyfills({
+      globals: {
+        global: true,
+      },
+    }),
+    commonjs(),
+  ],
+  server: {
+    port: 3000,
+    host: '0.0.0.0'
+  },
+  preview: {
+    port: 3000,
+    host: '0.0.0.0'
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    rollupOptions: {
+      external: ['require']
+    }
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+  },
 })

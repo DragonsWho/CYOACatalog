1413b9d12f946d813bcd343ca505f525b202b8c0
sgvsbg8gv29ybgqk <sgvsbg8gv29ybgqk@gmail.com>
2024-09-10 19:48:14 -0500

    feat: added tailwind

--- flake.nix
+++ flake.nix
@@ -10,31 +10,12 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        pocketbase = pkgs.python3Packages.buildPythonPackage {
-          pname = "pocketbase";
-          version = "0.12.1";
-          src = pkgs.fetchPypi {
-            pname = "pocketbase";
-            version = "0.12.1";
-            sha256 = "74c64ae894802419b372f6137342d4739383cb027085059b812988cb1d0b78d8";
-          };
-          doCheck = false;
-          pyproject = true;
-          pythonRelaxDeps = [ "httpx" ];
-          buildInputs = [ pkgs.python3Packages.poetry-core ];
-          dependencies = [ pkgs.python3Packages.httpx ];
-        };
       in
       {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
-            (python3.withPackages (ps: with ps; [
-              pocketbase
-              bcrypt
-              requests
-              pillow
-              xxhash
-            ]))
+            bun
+            go
           ];
           shellHook = ''
             export GIT_AUTHOR_EMAIL='sgvsbg8gv29ybgqk@gmail.com'
